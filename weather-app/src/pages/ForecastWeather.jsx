f652711c2e56d7897282711b33be096c510412b7
Jack Ivison <jack.ivison@datalex.com>
2025-01-14 12:49:30 +0000

    Added weather message and error messages to the forecast page

--- weather-app/src/pages/ForecastWeather.jsx
+++ weather-app/src/pages/ForecastWeather.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchBar from "../components/SearchBar";
+import WeatherMessage from "../components/WeatherMessage";
 
 export default function ForecastWeather() {
+    const [forecast, setForecast] = useState(null);
+    const [city, setCity] = useState('');
+    const [error, setError] = useState('');
+    const [weatherMessage, setWeatherMessage] = useState('');
+
     const fetchForecast = async () => {
         try {
             const API_KEY = import.meta.env.VITE_APP_WEATHER_API_KEY;
             const response = await axios.get(
-                `https://api.weatherapi.com/v1/current.json`,
+                `https://api.weatherapi.com/v1/forecast.json`,
                 {
                     params: {
                         key: API_KEY,
@@ -14,21 +20,25 @@ export default function ForecastWeather() {
                     },
                 }
             );
-            setWeather(response.data);
+            setForecast(response.data.forecast.forecastday);
 
             const location = response.data.location
             const msg = `${location.name}, ${location.country}`;
             setWeatherMessage(msg);
             setError('');
         } catch (err) {
-            setWeather(null);
+            setForecast(null);
             setError('Enter a valid city');
         }
     };
 
     return (
         <div>
-            <SearchBar></SearchBar>
+            <SearchBar onSearch={fetchForecast} setCity={setCity}></SearchBar>
+            <div>
+                <WeatherMessage weather={forecast} weatherMessage={weatherMessage} />
+            </div>
+            <div className='error-message'>{error}</div>
         </div>
     );
 }
